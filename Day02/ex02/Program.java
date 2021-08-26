7e15236062a035a7a7d10d93434c92c509ed14a0
horse <airodionov@protonmail.com>
2021-08-26 17:19:52 +0300

    Finished Day02 ex02

--- Day02/ex02/Program.java
+++ Day02/ex02/Program.java
@@ -3,6 +3,22 @@ import java.io.*;
 import java.nio.file.*;
 
 public class Program {
+    public enum Size {
+        B,
+        KB,
+        MB,
+        GB,
+        TB;
+
+        public boolean hasNext() {
+            return ordinal() < (values().length - 1);
+        }
+
+        public Size next() {
+            return values()[ordinal() + 1];
+        }
+    }
+
     public static void main(String[] args) {
         if ((args.length != 1) || (args.length == 1
                 && !args[0].startsWith("--current-folder="))) {
@@ -20,13 +36,16 @@ public class Program {
             System.out.println("Current folder is not a directory");
             return;
         }
-        
-        System.out.println(curPath.toString());
+
+        curPath = curPath.toAbsolutePath().normalize();
+
+//        System.out.println(curPath.toString());
 
         Scanner s = new Scanner(System.in);
 
         while (true) {
-            String input = s.nextLine();
+            System.out.println(curPath.toString());
+            String input = s.nextLine().trim();
 
             if (input.equals("exit")) {
                 return;
@@ -35,11 +54,80 @@ public class Program {
             if (input.equals("ls")) {
                 for (File f : curPath.toFile().listFiles()) {
                     try {
-                        System.out.printf("%s %d B\n", f.getName(), Files.size(f.toPath()));
+                        Size eSize = Size.B;
+//                        double size = Files.size(f.toPath());
+//                        while (eSize.hasNext()) {
+//                            double newSize = size / 1024f;
+//                            if (newSize <= 0.99) {
+//                                break;
+//                            }
+//                            size = newSize;
+//                            eSize = eSize.next();
+//                        }
+                        System.out.printf("%s %d %s\n", f.getName(), Files.size(f.toPath()), "B");
                     } catch (IOException e) {
                         System.err.println(e.getMessage());
                     }
                 }
+                continue;
+            }
+
+            if (input.equals("cd")) {
+                System.out.println("cd: no FOLDER_NAME specified");
+                continue;
+            } else if (input.startsWith("cd ")) {
+                String pathStr = input.substring(3);
+                Path path = curPath.resolve(Paths.get(pathStr)).normalize();
+                File pathFile = path.toFile();
+                if (!pathFile.exists()) {
+                    System.err.printf("cd: '%s' does not exists\n", pathStr);
+                    continue;
+                }
+                if (!pathFile.isDirectory()) {
+                    System.err.printf("cd: '%s' is not a directory\n", pathStr);
+                    continue;
+                }
+                curPath = path;
+                continue;
+            }
+
+            if (input.equals("mv")) {
+                System.out.println("mv: no WHAT and WHERE specified");
+                continue;
+            } else if (input.startsWith("mv ")) {
+                String cmd = input.substring(3);
+                String[] split = cmd.split("\\s+");
+                if (split.length == 1) {
+                    System.out.println("mv: no WHERE specified");
+                    continue;
+                } else if (split.length > 2) {
+                    System.out.println("mv: too many arguments");
+                    continue;
+                }
+
+                Path moveFrom = curPath.resolve(Paths.get(split[0])).normalize();
+                File moveFromFile = moveFrom.toFile();
+                if (!moveFromFile.exists()) {
+                    System.err.printf("mv: '%s' does not exists\n", split[0]);
+                    continue;
+                }
+                if (moveFromFile.isDirectory()) {
+                    System.err.printf("mv: '%s' is a directory\n", split[0]);
+                    continue;
+                }
+
+                Path moveTo = curPath.resolve(Paths.get(split[1])).normalize();
+                File moveToFile = moveTo.toFile();
+                if (moveToFile.exists() && moveToFile.isFile()) {
+                    System.err.printf("mv: '%s' already exists\n", split[1]);
+                    continue;
+                }
+                if (moveToFile.isDirectory()) {
+                    moveTo = Paths.get(moveTo.toString() + File.separator + moveFromFile.getName());
+                    moveToFile = moveTo.toFile();
+                }
+                
+                moveFromFile.renameTo(moveToFile);
             }
         }
     }
